c3f81ad814759d901db7edf7a90edde5e1e22674
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-13 16:24:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,9 +72,9 @@ class Dog
   end
   
   def update
-  #   UPDATE table_name
-  #   SET column1 = value1, column2 = value2, ...
-  #   WHERE condition;
+    UPDATE table_name
+    SET column1 = value1, column2 = value2, ...
+    WHERE condition;
     
   #   DB[:conn].execute(id: ,
  end
