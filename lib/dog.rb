86a64d20886fd57d49277df92e07be0773f97456
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-13 16:22:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,11 +71,12 @@ class Dog
     self.new(id: doggo[0], name: doggo[1], breed: [2])
   end
   
-  # def self.update
+  def self.update
   #   UPDATE table_name
   #   SET column1 = value1, column2 = value2, ...
   #   WHERE condition;
     
   #   DB[:conn].execute(id: ,
+ end
   
 end 
