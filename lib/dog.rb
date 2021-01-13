111803a087fdbbe13ad558d3b1ea786885514a4a
Chris Houghton <chrishoughton2434@gmail.com>
2021-01-13 15:13:52 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,51 @@
+class Dog 
+  
+  attr_accessor :name, :breed 
+  attr_reader :id 
+  
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+    dog = <<-SQL
+    CREATE TABLE dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT)
+    SQL
+    
+    DB[:conn].execute(dog)
+  end
+  
+  def self.drop_table
+    DB[:conn].execute('DROP TABLE dogs')
+  end
+  
+  def save
+    # binding.pry
+    dog = <<-SQL 
+    INSERT INTO dogs (name, breed)
+    VALUES (?,?)
+    SQL
+    
+    DB[:conn].execute(dog, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+  
+  def self.create(attributes)
+    doggo = new(attributes)
+    doggo.save
+    doggo
+  end
+  
+  def self.new_from_db(pupper)
+    binding.pry
+  end
+  
+  
+  
+end 
